34ce2dbe002e9f4831da064980b596ca50f492f4
Ibrahim Kouma <69568495+piincher@users.noreply.github.com>
2021-10-17 19:33:15 +0800

    antd model

--- pages/register.js
+++ pages/register.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { toast } from 'react-toastify';
 import axios from 'axios';
 import FormInput from '../components/FormInput';
+import { Modal } from 'antd';
+import Link from 'next/link';
 
 import ResetPasswordQuestion from '../utils/reset';
 const Register = () => {
@@ -67,6 +69,16 @@ const Register = () => {
 							<button className="btn btn-primary col-12">submit</button>
 						</div>
 					</form>
+				</div>
+			</div>
+			<div className="row">
+				<div className="col">
+					<Modal title="congratulation" visible={ok} onCancel={() => setOk(false)} footer={null}>
+						<p>you have successfull register</p>
+						<Link href="/login">
+							<a className="btn btn-primary btn-sm">Login</a>
+						</Link>
+					</Modal>
 				</div>
 			</div>
 		</div>
