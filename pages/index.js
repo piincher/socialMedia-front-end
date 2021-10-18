bb832185a16e440ec77aeb784eb4f385cc4cc03b
Ibrahim Kouma <69568495+piincher@users.noreply.github.com>
2021-10-18 13:24:19 +0800

    display in the home to test

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+import { useContext } from 'react';
+import { UserContext } from '../context';
+
 const Home = () => {
+	const [ state, setState ] = useContext(UserContext);
 	return (
 		<div className="container">
 			<div className="row">
 				<div className="col">
 					<h1>home page </h1>
+					{JSON.stringify(state)}
 					<img src="/images/deno.jpeg" alt="" />
 				</div>
 			</div>
