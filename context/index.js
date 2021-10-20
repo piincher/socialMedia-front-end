1990d3885e320dd93d0746825ea8c78dc2f44cfb
Ibrahim Kouma <69568495+piincher@users.noreply.github.com>
2021-10-20 16:07:17 +0800

    axios receptors

--- context/index.js
+++ context/index.js
@@ -1,5 +1,6 @@
 import { useState, createContext, useEffect } from 'react';
-
+import { useRouter } from 'next/router';
+import axios from 'axios';
 const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
@@ -11,6 +12,20 @@ const UserProvider = ({ children }) => {
 	useEffect(() => {
 		setState(JSON.parse(window.localStorage.getItem('auth')));
 	}, []);
+	const router = useRouter();
+	axios.interceptors.response.use(
+		function(response) {
+			return response;
+		},
+		function(error) {
+			let res = error.response;
+			if (res.status === 401 && res.config && !res.config.__isRetrRequest) {
+				setState(null);
+				window.localStorage.removeItem('auth');
+				router.push('/login');
+			}
+		}
+	);
 
 	return <UserContext.Provider value={[ state, setState ]}>{children}</UserContext.Provider>;
 };
