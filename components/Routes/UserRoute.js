88d7aac7307e761f2844a36593e71ca600aa7d11
Ibrahim Kouma <69568495+piincher@users.noreply.github.com>
2021-10-20 13:34:34 +0800

    wrapper component for authorization create

---
+++ components/Routes/UserRoute.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { SyncOutlined } from '@ant-design/icons';
+import { UserContext } from '../../context';
+
+const UserRoute = ({ children }) => {
+	const [ ok, setOk ] = useState(false);
+	const [ state ] = useContext(UserContext);
+	const router = useRouter();
+
+	useEffect(
+		() => {
+			if (state && state.token) getCurrentUser();
+		},
+		[ state && state.token ]
+	);
+
+	const getCurrentUser = async () => {
+		try {
+			const { data } = await axios.get(`${process.env.NEXT_PUBLIC_API}/current-user`, {
+				headers: {
+					Authorization: `Bearer ${state.token}`
+				}
+			});
+			if (data.ok) setOk(true);
+		} catch (error) {
+			router.push('/login');
+		}
+	};
+
+	process.browser &&
+		state === null &&
+		setTimeout(() => {
+			getCurrentUser();
+		}, 1000);
+
+	return !ok ? (
+		<SyncOutlined spin className="d-flex justify-content-center display-1 text-primary p-5" />
+	) : (
+		<React.Fragment>{children}</React.Fragment>
+	);
+};
+
+export default UserRoute;
