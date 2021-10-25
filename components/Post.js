f582f95da90686de9e62b76c86731f2cbb8ca7d0
Ibrahim Kouma <69568495+piincher@users.noreply.github.com>
2021-10-25 16:30:07 +0800

    render the post

--- components/Post.js
+++ components/Post.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import renderHTML from 'react-render-html';
+import moment from 'moment';
+import { Avatar } from 'antd';
 const Post = ({ post }) => {
 	return (
 		<React.Fragment>
 			<div className="card-header">
-				<div>image /name /data</div>
+				<Avatar size={40}>{post.postedBy.name[0]}</Avatar>
+				<span className="pt-2 ml-3" style={{ marginLeft: '1rem' }}>
+					{post.postedBy.name}
+				</span>
+				<span className="pt-2 mr-3" style={{ marginLeft: '1rem' }}>
+					{moment(post.createdAt).fromNow()}
+				</span>
 			</div>
-			<div className="card-body">coneten</div>
-			<div className="card-footer">like /unlike comment</div>
+			<div className="card-body">{renderHTML(post.content)}</div>
+			<div className="card-footer">
+				<img src={post.image && post.image.url} alt={post.postedBy.name} />
+				<div>like comlike /unment</div>
+			</div>
 		</React.Fragment>
 	);
 };
