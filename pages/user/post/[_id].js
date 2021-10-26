843747613ca1dab801c93e7cfa6b206a1f344fd6
Ibrahim Kouma <69568495+piincher@users.noreply.github.com>
2021-10-26 19:52:55 +0800

    single post edit with single post

--- pages/user/post/[_id].js
+++ pages/user/post/[_id].js
@@ -1,8 +1,29 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { useRouter } from 'next/router';
+import { toast } from 'react-toastify';
+
 const EditPost = () => {
+	const [ post, setPost ] = useState({});
 	const router = useRouter();
 	const _id = router.query._id;
 
-	return <div>jelo{_id}</div>;
+	useEffect(
+		() => {
+			if (_id) fetchPost();
+		},
+		[ _id ]
+	);
+
+	const fetchPost = async () => {
+		try {
+			const { data } = await axios.get(`/user-post/${_id}`);
+
+			setPost(data);
+		} catch (error) {
+			toast.error('try again ');
+		}
+	};
+	return <div>{JSON.stringify(post, null, 4)}</div>;
 };
 export default EditPost;
